d64779e1fcb7afe241d04c340764b8aef994135c
Chyczewski Maciej <chyczewski.maciej@gmail.com>
2018-08-16 23:37:18 +0200

    Moved ClockFactory to test project

--- GenericBuffer.Tests/ClockFactory.cs
+++ GenericBuffer.Tests/ClockFactory.cs
@@ -1,6 +1,7 @@
 ﻿using System;
+using System.Threading;
 
-namespace GenericBuffer.Core
+namespace GenericBuffer.Tests
 {
     public class ClockFactory
     {
@@ -35,5 +36,25 @@ namespace GenericBuffer.Core
                 }
             };
         }
+    }
+
+    public class IncrementalClock
+    {
+        private long _tick_;
+        private readonly long _step_;
+
+        public long CurrentTick => _tick_;
+        public DateTime CurrentDateTime => new DateTime(_tick_);
+
+        public IncrementalClock(DateTime startDate) : this(startDate.Ticks) { }
+        public IncrementalClock(DateTime startDate, TimeSpan step) : this(startDate.Ticks, step.Ticks) { }
+
+        public IncrementalClock(long startTick = 0, long step = 1)
+        {
+            _tick_ = startTick;
+            _step_ = step;
+        }
+
+        public DateTime GetNextDateTime() => new DateTime(Interlocked.Add(ref _tick_, _step_));
     }
 }
