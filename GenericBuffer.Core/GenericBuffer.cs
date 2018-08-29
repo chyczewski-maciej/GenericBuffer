91b050f72d9ded4778cb71d241008a8724c725f7
Chyczewski Maciej <chyczewski.maciej@gmail.com>
2018-08-29 07:42:29 +0200

    Added Func<T,T> factory

--- GenericBuffer.Core/GenericBuffer.cs
+++ GenericBuffer.Core/GenericBuffer.cs
@@ -9,26 +9,27 @@ namespace GenericBuffer.Core
         private readonly Func<DateTime> _clock_;
 
         private readonly object _locker_ = new object();
-        private DateTime validUntil = DateTime.MinValue;
-        private T buffer;
+        private DateTime _validUntil = DateTime.MinValue;
+        private T _buffer;
 
         public GenericBuffer(Func<T> factory, TimeSpan bufferingPeriod) : this(factory, bufferingPeriod, () => DateTime.Now) { }
-        public GenericBuffer(Func<T> factory, TimeSpan bufferingPeriod, Func<DateTime> clock) : this(_ => factory(), bufferingPeriod, clock) { }
-        public GenericBuffer(Func<T, T> factory, TimeSpan bufferingPeriod) : this(factory, bufferingPeriod, () => DateTime.Now) { }
+        public GenericBuffer(Func<T> factory, TimeSpan bufferingPeriod, Func<DateTime> clock) : this(_ => factory(), bufferingPeriod, default, clock) { }
+        public GenericBuffer(Func<T, T> factory, TimeSpan bufferingPeriod, T initialValue) : this(factory, bufferingPeriod, initialValue, () => DateTime.Now) { }
 
-        public GenericBuffer(Func<T, T> factory, TimeSpan bufferingPeriod, Func<DateTime> clock)
+        public GenericBuffer(Func<T, T> factory, TimeSpan bufferingPeriod, T initialValue, Func<DateTime> clock)
         {
             _factory_ = factory ?? throw new ArgumentNullException(nameof(factory));
             _bufferingPeriod_ = bufferingPeriod;
             _clock_ = clock;
+            _buffer = initialValue;
         }
 
         public void Reset()
         {
             lock (_locker_)
             {
-                buffer = default;
-                validUntil = DateTime.MinValue;
+                _buffer = default;
+                _validUntil = DateTime.MinValue;
             }
         }
 
@@ -36,22 +37,22 @@ namespace GenericBuffer.Core
         {
             lock (_locker_)
             {
-                buffer = _factory_(buffer);
-                validUntil = NewValidUntil();
+                _buffer = _factory_(_buffer);
+                _validUntil = NewValidUntil();
             }
 
-            return buffer;
+            return _buffer;
         }
 
         public T GetValue()
         {
-            if (_clock_() < validUntil)
-                return buffer;
+            if (_clock_() < _validUntil)
+                return _buffer;
 
             lock (_locker_)
             {
-                if (_clock_() < validUntil)
-                    return buffer;
+                if (_clock_() < _validUntil)
+                    return _buffer;
 
                 return ForceRefresh();
             }
